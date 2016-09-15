65a8be9a77d0f84779b9789cac72257a8381691e
Robin Dousse <rodousse>
2016-09-15 16:08:12 +0200

    Ajout de save.h et création d la classe SaveSystem (PAUSE)

---
+++ save.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <fstream> 
+
+using namespace std;
+
+class SaveSystem
+{
+private:
+	// Reference Vers le tableau
+	// reference sur le score
+
+public:
+	void save(int **tab, int score);
+};
